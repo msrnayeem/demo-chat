474c9b8bad7890f5ac04d2389a54f20f61e32a21
Msr Nayeem <msrnayeem@gmail.com>
2023-07-28 08:33:28 +0600

    msg

--- resources/views/msg.blade.php
+++ resources/views/msg.blade.php
@@ -43,7 +43,7 @@
 }
 .messenger-container{
   z-index: 333;
-  width: 15%;
+  width: 18%;
   height: 250px;
   cursor: default;
   background-color:#ebebeb;
@@ -51,9 +51,9 @@
   top: 47%; /* This will position the messenger panel below the messenger icon */
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add some shadow for a better appearance */
   border-radius: 8px;
-  display: none;
+  display: flex;
   flex-direction: column;
-  right: 10%;
+  right: 8%;
       
 }
 .profile-container{
@@ -248,35 +248,8 @@
                               <div class="top">
                                   <h2>Chat</h2>
                               </div>
-                              <div class="main">
-                                  				<!--for loop to print message-->
-
-                                @for($i=0; $i<=6; $i++)
-                                <!--first message-->
-                                <div class="container px-0 pb-1 px-2">
-
-                                  <div class="row mx-0 bg-white rounded">
-                                    <div class="col-auto px-1">
-                                      <img src="{{ asset('uploads/1.jpg') }}" alt="User 1 Avatar" class="avatar">
-                                    </div>
-
-                                    <div class="col px-0 mx-1">
-                                      <div class="row mx-0">
-                                        <div class="col-5 px-0 text-start">
-                                            name {{$i}}
-                                        </div>
-                                        <div class="col-7 px-0 text-end fw-lighter ">
-                                        <small> 3{{$i}} min ago</small>
-                                        </div>
-                                      </div>
-                                      <div class="row mx-0 text-start">
-                                          <p class="my-0 px-0 fw-lighter">this is message {{$i}}...</p>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                                @endfor
-                                  <!-- Add more rows for other messages -->
+                              <div class="main">  
+                                <p>No messages yet</p>
                               </div>
                               <div class="footer">
                                   <a href="#" style="text-decoration: underline; color:black; font-size: 12px;">all messages</a>
@@ -530,12 +503,68 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
   $(document).ready(function() {
-    // Toggle containers when clicking on icons
-    $('#messenger').on('click', function() {
-      $('.profile-container').hide();
-      $('.notification-container').hide();
-      $('.messenger-container').toggle();
+  $('#messenger').on('click', function() {
+  $('.profile-container').hide();
+  $('.notification-container').hide();
+  var messengerContainer = $('.messenger-container');
+
+  // Check if the messenger container is hidden
+  if (messengerContainer.is(':hidden')) {
+    $.ajax({
+      url: '/get',
+      method: 'GET',
+      success: function(data) {
+        console.log('Received messages:', data);
+
+        if (data.status === true && data.data.length > 0) {
+          // Clear any existing content in the messenger container
+          messengerContainer.find('.main').empty();
+
+          data.data.forEach(function(message) {
+            var receiverName = message.receiver.name.split(' ')[0];
+            var body = message.body;
+            var truncatedBody = body.length > 20 ? body.substring(0, 20) + '...' : body;
+            var timestamp = calculateTimeAgo(message.created_at);
+            var messageHTML = `
+              <div class="container px-0 pb-1 px-2">
+                <div class="row mx-0 bg-white rounded">
+                  <div class="col-auto px-1">
+                    <img src="{{ asset('uploads/1.jpg') }}" alt="User 1 Avatar" class="avatar">
+                  </div>
+                  <div class="col px-0 mx-1">
+                    <div class="row mx-0">
+                      <div class="col-5 px-0 text-start">${receiverName}</div>
+                      <div class="col-7 px-0 text-end fw-lighter">
+                        <small>${timestamp}</small>
+                      </div>
+                    </div>
+                    <div class="row mx-0 text-start">
+                      <p class="my-0 px-0 fw-lighter">${truncatedBody}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `;
+
+            messengerContainer.find('.main').append(messageHTML);
+          });
+
+          // Show the messenger container after adding the messages
+          messengerContainer.toggle();
+        } else {
+          console.log('No messages found');
+        }
+      },
+      error: function(xhr, status, error) {
+        console.error('Error fetching messages:', error);
+      }
     });
+  } else {
+    // If the messenger container is already open, just toggle it to hide it
+    messengerContainer.toggle();
+  }
+});
+
 
     $('#notification').on('click', function() {
       $('.messenger-container').hide();
@@ -559,6 +588,33 @@
       }
     });
   });
+  // Function to calculate the time ago
+function calculateTimeAgo(createdAt) {
+  var now = new Date();
+  var createdDate = new Date(createdAt);
+
+  var timeDiff = now - createdDate;
+  var secondsDiff = Math.floor(timeDiff / 1000);
+
+  if (secondsDiff < 60) {
+    return secondsDiff + ' seconds ago';
+  } else if (secondsDiff < 3600) {
+    var minutesDiff = Math.floor(secondsDiff / 60);
+    return minutesDiff + ' minutes ago';
+  } else if (secondsDiff < 86400) {
+    var hoursDiff = Math.floor(secondsDiff / 3600);
+    return hoursDiff + ' hours ago';
+  } else if (secondsDiff < 2592000) {
+    var daysDiff = Math.floor(secondsDiff / 86400);
+    return daysDiff + ' days ago';
+  } else if (secondsDiff < 31536000) {
+    var monthsDiff = Math.floor(secondsDiff / 2592000);
+    return monthsDiff + ' months ago';
+  } else {
+    var yearsDiff = Math.floor(secondsDiff / 31536000);
+    return yearsDiff + ' years ago';
+  }
+}
 </script>
 
 
@@ -567,5 +623,3 @@
 
 </html>
  
-
-
